11f1cd73498d6a91d3e49f803daa3d9ff4abc92c
Eldos <eldos.baktybekov@gmail.com>
2023-09-30 16:13:08 +0600

    :necktie: Check if user already exists in contacts list

--- src/services/contacts.py
+++ src/services/contacts.py
@@ -1,4 +1,12 @@
-__all__ = ('can_create_new_contact', 'compute_new_contact_price')
+__all__ = (
+    'can_create_new_contact',
+    'compute_new_contact_price',
+    'is_user_already_contact',
+)
+
+from collections.abc import Iterable
+
+from models import Contact
 
 
 def can_create_new_contact(*, contact_price: int, balance: int) -> bool:
@@ -6,4 +14,12 @@ def can_create_new_contact(*, contact_price: int, balance: int) -> bool:
 
 
 def compute_new_contact_price(contacts_count: int) -> int:
-    return 100 * 2 ** contacts_count
+    return 100 * (2 + contacts_count) // 2 * (contacts_count + 1)
+
+
+def is_user_already_contact(
+        *,
+        user_id: int,
+        contacts: Iterable[Contact],
+) -> bool:
+    return any(contact.to_user.id == user_id for contact in contacts)
