888b8b17921387a6b13d22aa6e6eed6003e31f1f
Eldos <eldos.baktybekov@gmail.com>
2023-09-30 13:16:48 +0600

    :memo: Add docstrings for working service utilities

--- src/services/work.py
+++ src/services/work.py
@@ -13,6 +13,7 @@ __all__ = (
 
 
 def get_operator_sign() -> str:
+    """Returns a random operator sign. Plus, minus or multiply."""
     return random.choice('+-*')
 
 
@@ -22,9 +23,11 @@ class ArithmeticProblem:
         self.expression = expression
 
     def compute_correct_answer(self) -> int:
+        """Computes the correct answer for the problem."""
         return eval(self.expression)
 
     def compute_reward_value(self) -> int:
+        """Computes the reward value for solving the problem."""
         operators_complexity_value = (
                 self.expression.count('+') * 3
                 + self.expression.count('-') * 5
@@ -35,6 +38,15 @@ class ArithmeticProblem:
 
     @classmethod
     def from_text(cls, text: str) -> Self:
+        """
+        Returns an arithmetic problem from a text.
+
+        Args:
+            text: The text to parse the arithmetic problem from.
+
+        Returns:
+            An arithmetic problem.
+        """
         expression = ArithmeticExpression(
             text.split('\n')[0]
             .removeprefix('❓ Сколько будет: ')
@@ -44,6 +56,7 @@ class ArithmeticProblem:
 
 
 def get_arithmetic_expression() -> ArithmeticExpression:
+    """Returns an arithmetic expression with random operators and operands."""
     expression = (
         f'{get_operator_sign()}'
         f'{random.randint(1, 10)}'
@@ -58,6 +71,7 @@ def get_arithmetic_expression() -> ArithmeticExpression:
 
 
 def get_arithmetic_problem() -> ArithmeticProblem:
+    """Returns an arithmetic problem with a random expression."""
     expression = get_arithmetic_expression()
     return ArithmeticProblem(expression)
 
@@ -68,6 +82,18 @@ def compute_final_reward(
         premium_multiplier: int | float,
         is_premium: bool,
 ) -> int:
+    """
+    Computes the final reward value based on the premium status of the user.
+
+    Keyword Args:
+        reward_value: The reward value to compute.
+        premium_multiplier: The multiplier to apply to the reward value if the
+            user is premium.
+        is_premium: Whether the user is premium or not.
+
+    Returns:
+        The final reward value.
+    """
     if is_premium:
         return reward_value * premium_multiplier
     return int(reward_value)
