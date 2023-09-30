e405d483c39200919e03ea946fc2efb6fed702cf
Eldos <eldos.baktybekov@gmail.com>
2023-09-30 15:48:26 +0600

    :necktie: Remove contacts limit for non-premium users, make it paid required

--- src/services/contacts.py
+++ src/services/contacts.py
@@ -1,9 +1,9 @@
-__all__ = ('can_create_new_contact',)
+__all__ = ('can_create_new_contact', 'compute_new_contact_price')
 
 
-def can_create_new_contact(
-        *,
-        contacts_count: int,
-        is_premium: bool,
-) -> bool:
-    return contacts_count < 5 or is_premium
+def can_create_new_contact(*, contact_price: int, balance: int) -> bool:
+    return balance >= contact_price
+
+
+def compute_new_contact_price(contacts_count: int) -> int:
+    return 100 * 2 ** contacts_count
