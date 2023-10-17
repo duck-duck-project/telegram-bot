0393e46dc2d61ba3dcf573e1abd87716ec4c30b2
Eldos <eldos.baktybekov@gmail.com>
2023-10-17 09:13:35 +0600

    :recycle: Refactor contact error handlers

--- src/handlers/contacts/errors.py
+++ src/handlers/contacts/errors.py
@@ -1,34 +1,26 @@
 from aiogram import Router
 from aiogram.filters import ExceptionTypeFilter
-from aiogram.types import Update
+from aiogram.types import ErrorEvent
 
 from exceptions import ContactDoesNotExistError, ContactAlreadyExistsError
 
 __all__ = ('register_handlers',)
 
 
-async def on_contact_does_not_exist_error(
-        update: Update,
-        _: ContactDoesNotExistError,
-) -> bool:
+async def on_contact_does_not_exist_error(event: ErrorEvent) -> None:
     text = '😔 Контакт не существует или был удален'
-    if update.message is not None:
-        await update.message.answer(text)
-    if update.callback_query is not None:
-        await update.callback_query.answer(text, show_alert=True)
-    return True
+    if event.update.message is not None:
+        await event.update.message.answer(text)
+    if event.update.callback_query is not None:
+        await event.update.callback_query.answer(text, show_alert=True)
 
 
-async def on_contact_already_exists_error(
-        update: Update,
-        _: ContactAlreadyExistsError,
-) -> bool:
+async def on_contact_already_exists_error(event: ErrorEvent) -> None:
     text = '😶 Этот пользователь уже есть в ваших контактах'
-    if update.message is not None:
-        await update.message.answer(text)
-    if update.callback_query is not None:
-        await update.callback_query.answer(text, show_alert=True)
-    return True
+    if event.update.message is not None:
+        await event.update.message.answer(text)
+    if event.update.callback_query is not None:
+        await event.update.callback_query.answer(text, show_alert=True)
 
 
 def register_handlers(router: Router) -> None:
