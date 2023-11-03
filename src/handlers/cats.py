c26e97d022be29f9eab9420d0278a4a978e144c9
Eldos <eldos.baktybekov@gmail.com>
2023-11-03 09:59:27 +0600

    :sparkles: Fix cat's photo caching in Telegram servers

--- src/handlers/cats.py
+++ src/handlers/cats.py
@@ -1,8 +1,9 @@
 from aiogram import Router, F
 from aiogram.filters import StateFilter, Command, or_f
-from aiogram.types import Message
+from aiogram.types import Message, URLInputFile
 
 from repositories import BalanceRepository
+from services import BalanceNotifier
 
 __all__ = ('router',)
 
@@ -19,13 +20,12 @@ router = Router(name=__name__)
 async def on_send_cat_photo(
         message: Message,
         balance_repository: BalanceRepository,
+        balance_notifier: BalanceNotifier,
 ) -> None:
-    await balance_repository.create_withdrawal(
+    withdrawal = await balance_repository.create_withdrawal(
         user_id=message.from_user.id,
         amount=100,
         description='🐱 Фото котика',
     )
-    await message.reply_photo(
-        photo='https://cataas.com/cat',
-        caption='Вот тебе котик, держи :)',
-    )
+    await message.reply_photo(URLInputFile('https://cataas.com/cat'))
+    await balance_notifier.send_withdrawal_notification(withdrawal)
