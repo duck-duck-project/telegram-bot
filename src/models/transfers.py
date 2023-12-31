fdaa36e62867fd3dd251e5a9752e49200fef1793
Eldos <eldos.baktybekov@gmail.com>
2023-12-31 18:41:14 +0600

    :label: Remove transaction source model in all related fields

--- src/models/transfers.py
+++ src/models/transfers.py
@@ -3,7 +3,6 @@ from uuid import UUID
 
 from pydantic import BaseModel
 
-from models.transaction_sources import TransactionSource
 from models.users import UserPartial
 
 __all__ = ('Transfer',)
@@ -13,7 +12,6 @@ class Transfer(BaseModel):
     id: UUID
     sender: UserPartial
     recipient: UserPartial
-    source: TransactionSource
     amount: int
     description: str | None
     created_at: datetime
