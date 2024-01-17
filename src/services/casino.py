a821076da1c5f1102f70cdb0a533512474d5d3cb
Eldos <eldos.baktybekov@gmail.com>
2024-01-17 22:53:01 +0600

    :necktie: Swap black and red numbers on casino roulette

--- src/services/casino.py
+++ src/services/casino.py
@@ -19,10 +19,10 @@ __all__ = (
 
 
 class CasinoRoulette:
-    red: Final[set[int]] = {
+    black: Final[set[int]] = {
         2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35,
     }
-    black: Final[set[int]] = {
+    red: Final[set[int]] = {
         1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36,
     }
 
