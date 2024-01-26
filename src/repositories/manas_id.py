f0083acf6aad640314839ff7d759684b660b3606
Eldos <eldos.baktybekov@gmail.com>
2024-01-27 01:57:54 +0600

    :sparkles: Add closest birthdays service

--- src/repositories/manas_id.py
+++ src/repositories/manas_id.py
@@ -1,3 +1,5 @@
+from pydantic import TypeAdapter
+
 from exceptions.manas_id import ManasIdDoesNotExistError
 from models import ManasId
 from repositories.base import APIRepository
@@ -16,3 +18,33 @@ class ManasIdRepository(APIRepository):
 
         response_data = response.json()
         return ManasId.model_validate(response_data)
+
+    async def get_all(
+            self,
+            *,
+            limit: int | None = 100,
+            offset: int | None = 0,
+    ) -> list[ManasId]:
+        url = '/manas-id/'
+
+        manas_ids: list[ManasId] = []
+        while True:
+            request_query_params = {
+                'limit': limit,
+                'offset': offset,
+            }
+            response = await self._http_client.get(
+                url=url,
+                params=request_query_params,
+            )
+            response_data = response.json()
+
+            manas_ids += response_data['result']['manas_ids']
+
+            if response_data['result']['is_end_of_list_reached']:
+                break
+
+            offset += limit
+
+        type_adapter = TypeAdapter(list[ManasId])
+        return type_adapter.validate_python(manas_ids)
