7fb97649f6d1d011572f51f8a85b231b4d970e2e
Eldos <eldos.baktybekov@gmail.com>
2024-06-15 12:58:31 +0600

    :sparkles: Add mining statistics

--- src/handlers/mining.py
+++ src/handlers/mining.py
@@ -7,8 +7,11 @@ from repositories import MiningRepository
 from views import (
     MinedResourceView,
     MiningActionThrottledView,
+    MiningStatisticsView,
     reply_view,
 )
+
+__all__ = ('router',)
 
 router = Router(name=__name__)
 
@@ -30,4 +33,18 @@ async def on_mining(
     user_id = message.from_user.id
     mined_resource = await mining_repository.mine(user_id)
     view = MinedResourceView(mined_resource)
+    await reply_view(message=message, view=view)
+
+
+@router.message(
+    F.text.lower().in_({'шахта стата', 'шахта статистика'}),
+    StateFilter('*'),
+)
+async def on_mining_statistics(
+        message: Message,
+        mining_repository: MiningRepository,
+):
+    user_id = message.from_user.id
+    mining_statistics = await mining_repository.get_user_statistics(user_id)
+    view = MiningStatisticsView(mining_statistics)
     await reply_view(message=message, view=view)
