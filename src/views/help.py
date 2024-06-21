6b3fea6486e527442ae550bf29c63c988c13cb25
Eldos Baktybekov <97338193+usbtypec1@users.noreply.github.com>
2024-06-21 15:31:29 +0600

    Update help.py

--- src/views/help.py
+++ src/views/help.py
@@ -5,6 +5,20 @@ __all__ = ('HelpView',)
 
 class HelpView(View):
     text = (
-        '📚 Справочник по боту:'
-        ' <a href="https://eldos.notion.site/Duck-Duck-ff57b98762664d40a77119ea3daa6c5b?pvs=4">*ссылка*</a>'
+        '📚 Общая документация:'
+        ' <a href="https://duck-duck.super.site/">*ссылка*</a>\n\n'
+        '👤 Редактирование профиля:'
+        ' <a href="https://duck-duck.super.site/5181500d5a1b438f8841938e9cbde93b">*ссылка*</a>\n\n'
+        '🏅 Система наград:'
+        ' <a href="https://duck-duck.super.site/3faa8e800993432eac1a1a9bfac55fd8">*ссылка*</a>\n\n'
+        '🎲 Развлекательные действия:'
+        ' <a href="https://duck-duck.super.site/f838103ee1f745ecbbfbd3cfc54bb8f8">*ссылка*</a>\n\n'
+        '⛏️ Работа на шахте:'
+        ' <a href="https://duck-duck.super.site/c7a24b90200649eaab69b5510a9c636b">*ссылка*</a>\n\n'
+        '🗯️ Role-play действия:'
+        ' <a href="https://duck-duck.super.site/duck-duck-bot/role-play">*ссылка*</a>\n\n'
+        '🎰 Казино:'
+        ' <a href="https://duck-duck.super.site/f5b56423d0ae49c4b5d33939cca03f3e">*ссылка*</a>\n\n'
+        '🔋 Система здоровья, энергии и спорта:'
+        ' <a href="https://duck-duck.super.site/d40ac76c9f544335a2388cc7d1587abc">*ссылка*</a>'
     )
