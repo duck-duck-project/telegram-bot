44fef20a30f8ff0d756d94dc690147f840bdd309
Eldos <eldos.baktybekov@gmail.com>
2024-08-27 21:58:19 +0600

    :sparkles: Add chat id in mining create action

--- src/repositories/mining.py
+++ src/repositories/mining.py
@@ -6,9 +6,17 @@ __all__ = ('MiningRepository',)
 
 class MiningRepository(APIRepository):
 
-    async def mine(self, user_id: int) -> MinedResourceResult:
+    async def mine(
+            self,
+            user_id: int,
+            chat_id: int | None = None,
+    ) -> MinedResourceResult:
         url = '/mining/'
         request_data = {'user_id': user_id}
+
+        if chat_id is not None:
+            request_data['chat_id'] = chat_id
+
         response = await self._http_client.post(url, json=request_data)
         response_data = response.json()
 
