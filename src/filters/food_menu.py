e8e2f89d5012d3d4cbaeff2ee7a4b93f162c6c85
Eldos <eldos.baktybekov@gmail.com>
2024-09-16 21:39:57 +0600

    :children_crossing: Add new trigger-commands for yemek

--- src/filters/food_menu.py
+++ src/filters/food_menu.py
@@ -8,13 +8,27 @@ __all__ = (
 
 
 def food_menu_for_today_filter(message: Message) -> bool | dict:
-    if message.text.lower() != '/yemek today':
+    if message.text.lower() not in (
+            '/yemek today',
+            'yemek today',
+            'йемек сегодня',
+            'йемек на сегодня',
+            'еда на сегодня',
+            'меню на сегодня',
+    ):
         return False
     return {'days_skip_count': 0}
 
 
 def food_menu_for_tomorrow_filter(message: Message) -> bool | dict:
-    if message.text.lower() != '/yemek tomorrow':
+    if message.text.lower() not in (
+            '/yemek tomorrow',
+            'yemek tomorrow',
+            'йемек завтра',
+            'йемек на завтра',
+            'еда на завтра',
+            'меню на завтра',
+    ):
         return False
     return {'days_skip_count': 1}
 
@@ -24,7 +38,11 @@ def food_menu_for_n_days_filter(message: Message) -> bool | dict:
         command, days_skip_count = message.text.lower().split()
     except ValueError:
         return False
-    if command != '/yemek' or not days_skip_count.isdigit():
+
+    if command not in ('/yemek', 'yemek', 'йемек'):
+        return False
+
+    if not days_skip_count.isdigit():
         return False
 
     return {'days_skip_count': int(days_skip_count)}
