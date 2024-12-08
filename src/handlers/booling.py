1ff4d7b2d6ba2a0a7560b8ddfea21615cf34766f
Eldos <eldos.baktybekov@gmail.com>
2024-12-08 23:32:45 +0600

    :sparkles: Add booling

---
+++ src/handlers/booling.py
@@ -0,0 +1,35 @@
+import random
+
+from aiogram import F, Router
+from aiogram.filters import StateFilter
+from aiogram.types import Message
+
+__all__ = ('router',)
+
+router = Router(name=__name__)
+
+
+def to_snake_case(text: str) -> str:
+    whitespace = ' '
+    while whitespace * 2 in text:
+        text = text.replace(whitespace * 2, whitespace)
+    return text.lower().title().replace(whitespace, '_')
+
+
+@router.message(
+    F.reply_to_message,
+    F.message_id != F.reply_to_message.message_id,
+    StateFilter('*'),
+)
+async def on_bool_other_user(message: Message) -> None:
+    full_name = message.from_user.username or message.from_user.full_name
+    full_name = to_snake_case(full_name)
+    text = (
+        '<pre>'
+        '<code class="language-python">\n'
+        f'>>> bool({full_name})\n'
+        f'{random.choice(("True", "False"))}'
+        '</code>'
+        '</pre>'
+    )
+    await message.answer(text)
